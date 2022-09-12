e61de3caad2573816393d9e8891a3b25b1d6d378
Peiso Simone <simonepeiso@gmail.com>
2022-09-12 17:28:17 +0200

    Splash Screen Added

--- myApp/App.js
+++ myApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Lets work</Text>
+      <Text>Lets get started </Text>
       <StatusBar style="auto" />
     </View>
   );
