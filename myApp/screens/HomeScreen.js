981456647463196c4c33f12520a9a91cbebc6ac2
Peiso Simone <simonepeiso@gmail.com>
2022-09-14 08:51:18 +0200

    Styling added

--- myApp/screens/HomeScreen.js
+++ myApp/screens/HomeScreen.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Pressable
+  Pressable,
+  StyleSheet
 } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
@@ -12,29 +13,62 @@ const HomeScreen = ({ navigation }) => {
 
         <Pressable
           onPress={() => navigation.navigate('Details')}
-          style={{ backgroundColor: 'lightblue', padding: 10, marginBottom: 10, marginTop: 10 }}>
+          style={styles.inputView}>
         <Text>Details</Text>
       </Pressable>
 
       <Pressable
         onPress={() => navigation.navigate('Dashboard')}
-        style={{ backgroundColor: 'lightblue', padding: 10, marginBottom: 10, marginTop: 10 }}>
+        style={styles.inputView}>
        <Text>Dashboard</Text>
       </Pressable>
 
       <Pressable
         onPress={() => navigation.navigate('Splash')}
-        style={{ backgroundColor: 'lightblue', padding: 10, marginBottom: 10, marginTop: 10  }}>
+        style={styles.inputView}>
        <Text>Splash</Text>
       </Pressable>
 
       <Pressable
         onPress={() => navigation.navigate('Sign In')}
-        style={{ backgroundColor: 'lightblue', padding: 10, marginBottom: 10, marginTop: 10  }}>
+        style={styles.inputView}>
        <Text>Sign-In</Text>
       </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+ 
+  inputView: {
+    backgroundColor: "lightblue",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+ 
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+ 
+});
 
 export default HomeScreen;
--- myApp/screens/SignInScreen.js
+++ myApp/screens/SignInScreen.js
@@ -1,11 +1,87 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
-function SignInScreen({ navigation }) {
+function SignInScreen({ navigation }){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-      <Text> Sign-In</Text>
+    <View style={styles.container}>
+ 
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+ 
+  inputView: {
+    backgroundColor: "lightblue",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+ 
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "blue",
+  },
+});
 export default SignInScreen;
