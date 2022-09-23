32cf37210dc7e5229eaf384f550f2f3aa03d18b5
Peiso Simone <simonepeiso@gmail.com>
2022-09-23 06:18:10 +0200

    Splash Fixed

--- myApp/screens/SplashScreen.js
+++ myApp/screens/SplashScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, styles } from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
+import Splash2 from './../assets/splash2.png'
 
 const SplashScreen = ({navigation}) => {
   setTimeout(()=>{
@@ -8,13 +9,18 @@ const SplashScreen = ({navigation}) => {
 
   return (      
   <View style={styles.container}>
-    <div>
-      <img assets={splash2} alt=""/>
-    </div>
-    <Text style={{fontSize: 20, color:"#fff"}}>
-      Splash Screen
-    </Text>
+      <Image source={Splash2}/>
   </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   
+  }
+});
+
 export default SplashScreen;
